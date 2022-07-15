bb6a7a83a7bd6a21014bbd2371dd2cfae16542cd
Zeckie1 <109318174+Zeckie1@users.noreply.github.com>
2022-07-14 22:11:16 -1200

    Add files via upload

---
+++ lesssonjul14.py
@@ -0,0 +1,10 @@
+grade1 = int(input("Enter grade in ITE 306: "))
+grade2 = int(input("Enter grade in ITE 307: "))
+grade3 = int(input("Enter grade in ITE 308: "))
+
+
+ave= (grade1 + grade2 + grade3) / 3
+
+
+print("Average is: %.1f" %(ave))
+ 
